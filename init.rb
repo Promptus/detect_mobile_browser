102de473739eb922c31317be35833d482df87337
matti <matti@orgor.(none)>
2011-05-31 13:39:31 +0200

    port to rails3

--- init.rb
+++ init.rb
@@ -5,23 +5,18 @@ require File.join(File.dirname(__FILE__), '/lib/detect_mobile_browser/helpers')
 
 if defined?(ActionController)
 
-  require 'action_controller/cgi_process'
-  require 'action_controller/request'
+  require 'action_dispatch'
 
-  if defined?(ActionController::CgiRequest)
-    ActionController::CgiRequest.send :include, DetectMobileBrowser::Request
-  end
-
-  if defined?(ActionController::Request)
-    ActionController::Request.send :include, DetectMobileBrowser::Request
+  if defined?(ActionDispatch::Request)
+    ActionDispatch::Request.send :include, DetectMobileBrowser::Request
   end
 
   if Rails.env == "test"
     require 'action_controller/test_process'
-    ActionController::TestRequest.send :include, DetectMobileBrowser::Request
+    ActionDispatch::TestRequest.send :include, DetectMobileBrowser::Request
   end
 
-  ActionController::Base.send       :include, DetectMobileBrowser::Helpers
+  ActionController::Base.send :include, DetectMobileBrowser::Helpers
 
   class ActionController::Base
     before_filter :set_mobile_debug_session
@@ -39,10 +34,10 @@ if defined?(ActionController)
           session[debug] = nil
         end
       end
-      logger.debug "DetectMobile: Client has user agent: #{request.env['HTTP_USER_AGENT']}"
+      # logger.debug "DetectMobile: Client user agent: #{request.env['HTTP_USER_AGENT']}"
       logger.debug "DetectMobile: Client is mobile_device?: #{mobile_device?}. Really ? #{request.mobile_device?}."
-      logger.debug "DetectMobile: Client is oldschool_browser?: #{oldschool_browser?}. Really ? #{request.oldschool_browser?}."
-      logger.debug "DetectMobile: Client is which mobile_device: #{request.mobile_device}"
+      # logger.debug "DetectMobile: Client is oldschool_browser?: #{oldschool_browser?}. Really ? #{request.oldschool_browser?}."
+      # logger.debug "DetectMobile: Client is which mobile_device: #{request.mobile_device}"
       true
     end
   end
